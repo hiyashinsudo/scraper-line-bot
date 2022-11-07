6cd51fc06a77960a3114872e1af881c9a8e13064
Yoshida Yasuhiro <yasushi.adamant@gmail.com>
2022-11-08 00:41:05 +0900

    テストスクリプト追加

---
+++ headless_test.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+# ページによっては以下も必要になるよう
+# options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(options=options)  # 今は chrome_options= ではなく options=
+
+driver.get('https://www.google.com/')
+print(driver.title)
+
+search_box = driver.find_element_by_name("q")
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+print(driver.title)
+
+driver.save_screenshot('search_results.png')
+driver.quit()
